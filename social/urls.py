57430d86e6ed52a1fad10ab310470c04a4783348
changhee <change@hphk.kr>
2018-11-20 02:40:26 +0000

    장고 기본 auth.urls추가

--- social/urls.py
+++ social/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.HomePage.as_view(),name='home'),
     path('accounts/',include('accounts.urls')),
+    path('accounts/',include('django.contrib.auth.urls')),
     path('bye/',views.ByePage.as_view(),name='bye'),
 ]
