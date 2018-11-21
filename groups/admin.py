1edc749e857a045a846f5c28fdc272d31b8694de
changhee <change@hphk.kr>
2018-11-21 01:40:39 +0000

    admin 수정

--- groups/admin.py
+++ groups/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Group,GroupMember
 # Register your models here.
-admin.site.register(Group)
+class GroupMemberInline(admin.TabularInline):
+    model = GroupMember
+    
+class GroupAdmin(admin.ModelAdmin):
+    inlines=[GroupMemberInline]
+
+admin.site.register(Group, GroupAdmin)
 admin.site.register(GroupMember)
